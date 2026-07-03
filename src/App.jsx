b53e4573dc4d3ab21d854afbd0bbb78584d45611
Yasitha89 <ykanchanam@gmail.com>
2026-07-03 13:08:28 +0530

    login changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from "./pages/Login";
+import Login from "./pages/login";
 import Dashboard from "./pages/Dashboard";
 import LiveCount from "./pages/LiveCount";
 import Settings from "./pages/Settings";
